b0239fb18697946a31d3f62022e27ffc85c747b9
matydiaz434233 <102767133+matydiaz434233@users.noreply.github.com>
2023-08-18 15:52:05 -0300

    dotnet new mvc y run

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _net_inmobiliaria_Diaz_Matias.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
